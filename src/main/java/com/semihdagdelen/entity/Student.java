6b969c768ca4420daba5018cbe5e5ca897de14c0
Semih Dağdelen <semihdagdelen1837550@gmail.com>
2025-09-08 01:20:10 +0300

    springdatajpa ilk commit

---
+++ src/main/java/com/semihdagdelen/entity/Student.java
@@ -0,0 +1,23 @@
+package com.semihdagdelen.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "student")
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "birth_date")
+    private Date birthDate;
+}
